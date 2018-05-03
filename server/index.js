00ec822c929247a8de3798a342e17f2735584b48
DChan0319 <chaosstriker0319@gmail.com>
2018-05-02 18:53:46 -0700

    Basic env setup with package.json

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const app = express();
+const server = express();
 const PORT = 3000;
 
-app.use(express.static(path.resolve(__dirname, '../client/dist')));
+server.use(express.static(path.resolve(__dirname, '../client/dist')));
 
-app.use(morgan);
+server.use(morgan('dev'));
 
 server.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist')));
 
