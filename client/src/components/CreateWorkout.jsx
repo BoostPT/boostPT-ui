8c92dda6ab16c24abf7c1842f68ed38758dea918
Zach Gallagher <zachagallagher@gmail.com>
2018-05-07 16:51:30 -0700

    Got initial title, description to render on menu click

--- client/src/components/CreateWorkout.jsx
+++ client/src/components/CreateWorkout.jsx
@@ -10,10 +10,16 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import Checkbox from 'material-ui/Checkbox';
 import RaisedButton from 'material-ui/RaisedButton';
+import * as colors from 'material-ui/styles/colors';
 
 const addExerciseStyle = {
   marginTop: '20px',
   marginLeft: '20px'
+};
+
+const exerciseFormFloatStyle = {
+  color: '#9E9E9E',
+  fontSize: '14px'
 };
 
 const saveWorkoutStyle = {
@@ -21,38 +27,88 @@ const saveWorkoutStyle = {
   marginBottom: '20px'
 };
 
+const renderTextField = (hintText, floatingLabelText, multiLine = false, rowsMax = 1) => {
+  return (
+    <TextField
+      hintText={hintText}
+      floatingLabelText={floatingLabelText}
+      floatingLabelStyle={exerciseFormFloatStyle}
+      floatingLabelFixed={true}
+      multiLine={multiLine}
+      rowsMax={rowsMax}
+      underlineShow={false}
+    />
+  )
+};
+
 class CreateWorkout extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      exerciseForms: []
+    };
+    this.handleAddExerciseMenuClick = this.handleAddExerciseMenuClick.bind(this);
+  }
+
+  handleAddExerciseMenuClick(e) {
+    let exerciseForm = {
+      type: e.target.innerText
+    };
+    this.setState({ exerciseForms: [...this.state.exerciseForms, exerciseForm] });
+  }
+
+  renderAddExerciseButton() {
+    return (
+      <div className="add-exercise-menu">
+        <IconMenu
+          iconButtonElement={<FloatingActionButton backgroundColor={colors.grey600} style={addExerciseStyle} mini={true}><ContentAdd /></FloatingActionButton>}
+          anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+        >
+          <MenuItem onClick={this.handleAddExerciseMenuClick} primaryText="Warm-up" />
+          <MenuItem onClick={this.handleAddExerciseMenuClick} primaryText="Strength" />
+          <MenuItem onClick={this.handleAddExerciseMenuClick} primaryText="Cardio" />
+          <MenuItem onClick={this.handleAddExerciseMenuClick} primaryText="Stretch" />
+        </IconMenu>
+      </div>
+    )
+  }
+
+  renderExerciseForms() {
+
+    // const addWarmupFields = () => {
+    //   return (
+    //     {renderTextField('Exercise Name', `${exerciseForm.type} Exercise`)}
+    //   )
+    // };
+
+    return this.state.exerciseForms.map((exerciseForm, i) => {
+      return (
+        <div className="exercise-form" key={i}>
+          {renderTextField('Exercise Name', `${exerciseForm.type} Exercise`)}
+          {renderTextField('Enter Description (optional)', 'Description', true, 4)}
+          <Divider />
+        </div>
+      )
+    });
   }
 
   render() {
     return (
-      <Paper id="create-workout" zDepth={2}>
-        <h2 className="create-workout-header">Create Workout</h2>
-        <br />
-        <TextField className="create-wo-input" hintText="Workout name (optional)" underlineShow={false} />
-        <Divider />
-        <div className="add-exercise-menu">
-          <IconMenu
-            // iconButtonElement={<IconButton><AddButton style={addButtonStyle}>Add Exercise</AddButton></IconButton>}
-            iconButtonElement={<FloatingActionButton backgroundColor="grey" style={addExerciseStyle} mini={true}><ContentAdd /></FloatingActionButton>}
-            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-            targetOrigin={{horizontal: 'left', vertical: 'top'}}
-          >
-            <MenuItem primaryText="Warm-up" />
-            <MenuItem primaryText="Strength" />
-            <MenuItem primaryText="Cardio" />
-            <MenuItem primaryText="Stretch" />
-          </IconMenu>
+      <Paper id="create-workout-outer" zDepth={2}>
+        <div id="create-workout-inner">
+          <h2 className="create-workout-header">Create Workout</h2>
+          <br />
+          <TextField className="create-wo-input" hintText="Workout name (optional)" underlineShow={false} />
+          <Divider />
+          {this.renderExerciseForms()}
+          <Divider />
+          {this.renderAddExerciseButton()}
+          <div id="submit-wo-div">
+            <Checkbox label="Make Private" />
+            <RaisedButton backgroundColor={colors.grey800} label="Save Workout" labelColor={colors.yellow500} style={saveWorkoutStyle} />
+          </div>
         </div>
-
-        <br />
-        <div id="submit-wo-div">
-          <Checkbox label="Make Private" />
-          <RaisedButton backgroundColor="grey" label="Save Workout" labelColor="#FFEB3B" style={saveWorkoutStyle} />
-        </div>
-
       </Paper>
     )
   }
