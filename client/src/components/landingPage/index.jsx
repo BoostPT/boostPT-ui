6b6ded387d689342a633b8190f37a5ab30b9736e
jaba9531 <jakecbarber@gmail.com>
2018-05-08 13:28:18 -0700

    built ui for client tab

--- client/src/components/landingPage/index.jsx
+++ client/src/components/landingPage/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 import { Link } from 'react-router-dom';
 import RaisedButton from 'material-ui/RaisedButton';
 import * as colors from 'material-ui/styles/colors';
@@ -18,8 +20,72 @@ class ClientTab extends Component {
   }
   render() {
       return (
-          <div>
-              hey
+          <div className='wrapper'>
+              <div className='outerContainer'>
+                <div className='innerContainer'>
+                <div className="row">
+                  <div className="column">
+                  <form className="search">
+                    <input type="text" className="textbox" placeholder="Filter"></input>
+                    <input title="Search" value="" type="submit" className="button"/>
+                  </form>
+                </div>
+                  <div className="column">
+                    <button className="addClientButton" >Add a Client</button>
+                  </div>
+                  <div className="column">
+                    <div id="cardPlaceHolder">
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="column"><Card className = 'clientCard'>
+                    <CardText className ='clientCardText'
+                      // avatar="images/ok-128.jpg"
+                    > 
+                    <div className="name">Jake Barber</div>
+                    </CardText>
+                  </Card></div>
+                  <div className="column"><Card className = 'clientCard'>
+                    <CardText className ='clientCardText'
+                      // avatar="images/ok-128.jpg"
+                    > 
+                    <div className="name">Jake Barber</div>
+                    </CardText>
+                  </Card></div>
+                  <div className="column"><Card className = 'clientCard'>
+                    <CardText className ='clientCardText'
+                      // avatar="images/ok-128.jpg"
+                    > 
+                    <div className="name">Jake Barber</div>
+                    </CardText>
+                  </Card></div>
+                </div>
+                <div className="row">
+                  <div className="column"><Card className = 'clientCard'>
+                    <CardText className ='clientCardText'
+                      // avatar="images/ok-128.jpg"
+                    > 
+                    <div className="name">Jake Barber</div>
+                    </CardText>
+                  </Card></div>
+                  <div className="column"><Card className = 'clientCard'>
+                    <CardText className ='clientCardText'
+                      // avatar="images/ok-128.jpg"
+                    > 
+                    <div className="name">Jake Barber</div>
+                    </CardText>
+                  </Card></div>
+                  <div className="column"><Card className = 'clientCard'>
+                    <CardText className ='clientCardText'
+                      // avatar="images/ok-128.jpg"
+                    > 
+                    <div className="name">Jake Barber</div>
+                    </CardText>
+                  </Card></div>
+                </div>
+                </div>
+              </div>
           </div>
       );
   }
