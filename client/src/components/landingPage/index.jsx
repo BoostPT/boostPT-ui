2289a9e985f78e0f402f61c985b15cd8ebaa6c28
jaba9531 <jakecbarber@gmail.com>
2018-05-08 21:46:23 -0700

    trainer can create an unregistered user

--- client/src/components/landingPage/index.jsx
+++ client/src/components/landingPage/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import { Link } from 'react-router-dom';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -12,83 +11,6 @@ const toolbarStyle = {
 
 const toolbarTitleStyle = {
   color: colors.yellow500
-}
-
-class ClientTab extends Component {
-  constructor(props) {
-      super(props);
-  }
-  render() {
-      return (
-          <div className='wrapper'>
-              <div className='outerContainer'>
-                <div className='innerContainer'>
-                <div className="row">
-                  <div className="column">
-                  <form className="search">
-                    <input type="text" className="textbox" placeholder="Filter"></input>
-                    <input title="Search" value="" type="submit" className="button"/>
-                  </form>
-                </div>
-                  <div className="column">
-                    <button className="addClientButton" >Add a Client</button>
-                  </div>
-                  <div className="column">
-                    <div id="cardPlaceHolder">
-                    </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="column"><Card className = 'clientCard'>
-                    <CardText className ='clientCardText'
-                      // avatar="images/ok-128.jpg"
-                    > 
-                    <div className="name">Jake Barber</div>
-                    </CardText>
-                  </Card></div>
-                  <div className="column"><Card className = 'clientCard'>
-                    <CardText className ='clientCardText'
-                      // avatar="images/ok-128.jpg"
-                    > 
-                    <div className="name">Jake Barber</div>
-                    </CardText>
-                  </Card></div>
-                  <div className="column"><Card className = 'clientCard'>
-                    <CardText className ='clientCardText'
-                      // avatar="images/ok-128.jpg"
-                    > 
-                    <div className="name">Jake Barber</div>
-                    </CardText>
-                  </Card></div>
-                </div>
-                <div className="row">
-                  <div className="column"><Card className = 'clientCard'>
-                    <CardText className ='clientCardText'
-                      // avatar="images/ok-128.jpg"
-                    > 
-                    <div className="name">Jake Barber</div>
-                    </CardText>
-                  </Card></div>
-                  <div className="column"><Card className = 'clientCard'>
-                    <CardText className ='clientCardText'
-                      // avatar="images/ok-128.jpg"
-                    > 
-                    <div className="name">Jake Barber</div>
-                    </CardText>
-                  </Card></div>
-                  <div className="column"><Card className = 'clientCard'>
-                    <CardText className ='clientCardText'
-                      // avatar="images/ok-128.jpg"
-                    > 
-                    <div className="name">Jake Barber</div>
-                    </CardText>
-                  </Card></div>
-                </div>
-                </div>
-              </div>
-          </div>
-      );
-  }
 }
 
 class LandingPage extends Component {
