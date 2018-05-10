e55ec29fcca38864bb6bcfbdf2cf0e2fe6d5d15e
DChan0319 <chaosstriker0319@gmail.com>
2018-05-09 20:24:46 -0700

    Trying to add environment variables globally for client ui side

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -283,6 +283,7 @@ export const changeUserPicture = async (payload) => {
     await axios.put(signedUrl.data, payload.file[0], options);
 
     const result = await axios.put(`http://localhost:8000/api/users/${payload.user.id}/picture`, body);
+<<<<<<< 1597abc32d05525546d667333c3e0cb7f2e81923
     
 =======
 =======
@@ -389,6 +390,8 @@ export const loginUser = async (user) => {
 >>>>>>> Trying to add environment variables globally for client ui side
 =======
 >>>>>>> picture changes on bioPage, but data does not persist when navigating back
+=======
+>>>>>>> Trying to add environment variables globally for client ui side
     return {
       type: CHANGE_USER_PICTURE,
 <<<<<<< 52fe16c0a9805504aa3995186039f77bb2b7246e
