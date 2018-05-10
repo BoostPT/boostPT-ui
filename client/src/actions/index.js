fed060c1c0a648d67d75216d40bbe61b40745568
DChan0319 <chaosstriker0319@gmail.com>
2018-05-10 12:18:28 -0700

    picture changes on bioPage, but data does not persist when navigating back to the dashpage.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -301,11 +301,20 @@ export const changeUserPicture = async (payload) => {
 >>>>>>> picture changes on bioPage, but data does not persist when navigating back
     await axios.put(signedUrl.data, payload.file[0], options);
 
+<<<<<<< 502d2146fdb607e3f1650bdee711de17f6c37af7
 <<<<<<< 0dd19d593d89bfae31a342f3f7c01a11fa103bed
 <<<<<<< 8c20b07759c652f9508eb819bd5b7a619ba4fd2d
     //post to the database the link url and change the picture link in the url
 >>>>>>> Trying to add environment variables globally for client ui side
 =======
+=======
+<<<<<<< ab47a668d03699c482ebd270661bac20822ae4a9
+=======
+export const loginUser = async (user) => {
+  try {
+    const result = await axios.post('http://localhost:8000/api/auth/login', user);
+    console.log("login action",result);
+>>>>>>> picture changes on bioPage, but data does not persist when navigating back
 >>>>>>> picture changes on bioPage, but data does not persist when navigating back
     return {
       type: CHANGE_USER_PICTURE,
@@ -456,15 +465,23 @@ export const changeUserPicture = async (payload) => {
 
   try{
     const signedUrl = await axios.post('http://localhost:8000/api/aws/s3',picture);
-    await axios.put(signedUrl.data, payload.file[0], options);
-    await axios.put(`http://localhost:8000/api/users/${payload.user.id}/picture`, body);
 
+    await axios.put(signedUrl.data, payload.file[0], options);
+
+<<<<<<< ab47a668d03699c482ebd270661bac20822ae4a9
     //post to the database the link url and change the picture link in the url
 >>>>>>> Trying to add environment variables globally for client ui side
 >>>>>>> Trying to add environment variables globally for client ui side
     return {
       type: TRAINER_CLIENT_LIST,
       payload: result.data
+=======
+    const result = await axios.put(`http://localhost:8000/api/users/${payload.user.id}/picture`, body);
+
+    return {
+      type: CHANGE_USER_PICTURE,
+      payload: { username: payload.user.username, isTrainer: payload.user.isTrainer, id: payload.user.id, picture: result.data.pictureUrl}
+>>>>>>> picture changes on bioPage, but data does not persist when navigating back
     };
   } catch (err) {
     return (err);
