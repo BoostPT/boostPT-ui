0ffc9bb9311cd5bee49f494af410736a54f1e1cd
Zach Gallagher <zachagallagher@gmail.com>
2018-05-10 21:49:32 -0700

    Refactored dashPage by moving workout tab list so it can render on the right

--- client/src/components/dashPage/index.jsx
+++ client/src/components/dashPage/index.jsx
@@ -2,8 +2,10 @@ import React, {Component, Fragment} from 'react';
 import PropTypes from 'prop-types';
 import Navbar from './navbar.jsx';
 import {Tabs, Tab} from 'material-ui/Tabs';
+import { List, ListItem } from 'material-ui/List';
 
 import WorkoutsTab from './workoutsTab.jsx';
+import * as colors from "material-ui/styles/colors";
 
 const tabStyle = {
   standard: {
@@ -18,9 +20,42 @@ const tabStyle = {
   }
 };
 
+const workoutTabListStyle = {
+  standard: {
+    backgroundColor: colors.grey300
+  },
+  active: {
+    backgroundColor: colors.grey600
+  }
+};
+
 class DashPage extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      workoutTabActiveListItem: 0
+    };
+    this.handleWorkoutTabListItemSelect = this.handleWorkoutTabListItemSelect.bind(this);
+  }
+
+  handleWorkoutTabListItemSelect(e) {
+    this.setState({
+      workoutTabActiveListItem: parseInt(e.currentTarget.getAttribute('data'))
+    });
+  }
+
+  renderWorkoutTabList() {
+    const workoutTabItemStatus = Array(2).fill('').map((v, i) => this.state.workoutTabActiveListItem === i ? 'active' : 'standard');
+    return this.props.activeTab === 1 ? (
+      <div className="workouts-list-div">
+        <List className="workouts-list-select">
+          <ListItem primaryText="View Workouts" style={workoutTabListStyle[workoutTabItemStatus[0]]} onClick={this.handleWorkoutTabListItemSelect} data="0" disableTouchRipple={true}/>
+          <ListItem primaryText="Create Workout" style={workoutTabListStyle[workoutTabItemStatus[1]]} onClick={this.handleWorkoutTabListItemSelect} data="1" disableTouchRipple={true}/>
+        </List>
+      </div>
+    ) : (
+      null
+    );
   }
 
   render() {
@@ -39,7 +74,7 @@ class DashPage extends Component {
                 </div>
               </Tab>
               <Tab label="Workouts" style={tabStyle[tabStyles[1]]} onActive={this.props.handleTabSelect} disableTouchRipple={true}>
-                <WorkoutsTab user_id={this.props.userInfo.id} />
+                <WorkoutsTab activeListItem={this.state.workoutTabActiveListItem} user_id={this.props.userInfo.id} />
               </Tab>
               <Tab label="Clients" style={tabStyle[tabStyles[2]]} onActive={this.props.handleTabSelect} disableTouchRipple={true}>
                 <div>
@@ -47,6 +82,7 @@ class DashPage extends Component {
                 </div>
               </Tab>
             </Tabs>
+            {this.renderWorkoutTabList()}
           </div>
         </div>
     )
--- client/src/components/dashPage/workoutsTab.jsx
+++ client/src/components/dashPage/workoutsTab.jsx
@@ -1,60 +1,23 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { List, ListItem } from 'material-ui/List';
 import CreateWorkoutContainer from '../../containers/CreateWorkoutContainer.jsx';
-import * as colors from "material-ui/styles/colors";
-
-const listStyle = {
-  standard: {
-    backgroundColor: colors.grey300
-  },
-  active: {
-    backgroundColor: colors.grey600
-  }
-};
 
 class WorkoutsTab extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      activeListItem: 0
-    };
-    this.handleListItemSelect = this.handleListItemSelect.bind(this)
   }
 
-  handleListItemSelect(e) {
-    this.setState({
-      activeListItem: parseInt(e.currentTarget.getAttribute('data'))
-    });
-  }
-
-  renderContent() {
-    return this.state.activeListItem === 0 ? (
+  render() {
+    return this.props.activeListItem === 0 ? (
       <h1>INSERT VIEW WORKOUTS COMPONENT</h1>
     ) : (
       <CreateWorkoutContainer user_id={this.props.user_id} />
     );
   }
-
-  render() {
-    const itemStatus = Array(2).fill('').map((v, i) => this.state.activeListItem === i ? 'active' : 'standard');
-
-    return (
-      <Fragment>
-        {this.renderContent()}
-        <div className="workouts-list-div">
-          <List className="workouts-list-select">
-            <ListItem primaryText="View Workouts" style={listStyle[itemStatus[0]]} onClick={this.handleListItemSelect} data="0" disableTouchRipple={true}/>
-            <ListItem primaryText="Create Workout" style={listStyle[itemStatus[1]]} onClick={this.handleListItemSelect} data="1" disableTouchRipple={true}/>
-          </List>
-        </div>
-      </Fragment>
-
-    )
-  }
 }
 
 WorkoutsTab.propTypes = {
+  activeListItem: PropTypes.number.isRequired,
   user_id: PropTypes.number.isRequired
 };
 
