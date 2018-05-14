2c85080f7d7db3de94ad6001c7bc3ee832ae1f91
Zach Gallagher <zachagallagher@gmail.com>
2018-05-14 16:25:46 -0700

    Blacklisted auth error state from persisting, but it still appears on reload after bad login

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,18 @@
 import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import authReducer from './authReducer';
 import workoutsReducer from './workoutsReducer';
 import trainerReducer from './trainerReducer';
 
+const authPersistConfig = {
+  key: 'auth',
+  storage: storage,
+  blacklist: ['error']
+};
+
 const rootReducer = combineReducers({
-  auth: authReducer,
+  auth: persistReducer(authPersistConfig, authReducer),
   workoutsReducer,
   trainer: trainerReducer
 });
