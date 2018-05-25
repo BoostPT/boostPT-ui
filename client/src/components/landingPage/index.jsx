82cbb8cba5207433bb4dd3af3c9c515449f3303f
Zach Gallagher <zachagallagher@gmail.com>
2018-05-24 23:02:55 -0700

    Landing page section design

--- client/src/components/landingPage/index.jsx
+++ client/src/components/landingPage/index.jsx
@@ -10,7 +10,8 @@ import * as colors from 'material-ui/styles/colors';
 const authBtnLabelStyle = {
   fontFamily: 'Lato',
   fontSize: '1.2em',
-  textTransform: 'none'
+  textTransform: 'none',
+  color: '#EEE'
 };
 
 const authBtnStyle = {
@@ -62,12 +63,12 @@ class LandingPage extends Component {
         </div>
         <br />
         <h3 className="landing-page-header">Personal Training Client Management, Simplified</h3>
-        <div className="landing-page-subheader">
-          <div className="landing-page-col-grid">
+        <h2 className="landing-page-header" id="landing-page-subheader">For the professional trainer, or the dedicated athlete</h2>
+        <div className="landing-page-feature-list">
+          <div className="landing-page-subheader-grid">
             <div className="landing-page-col">
               <h3>Schedule Workouts</h3>
               <CalendarIcon className="landing-page-icon" style={iconStyle} />
-              <p className="landing-page-txt"></p>
             </div>
             <div className="landing-page-col">
               <h3>Log Exercises</h3>
@@ -83,7 +84,70 @@ class LandingPage extends Component {
             </div>
           </div>
         </div>
-        <span className="footer">Copyright &copy; 2018 BoostPT. All Rights Reserved.</span>
+        <div className="landing-page-image">
+          {/*<img src="FILL IN WITH PHOTO" />*/}
+        </div>
+        {/*CALENDAR PREVIEW*/}
+        <div className="landing-page-preview">
+          <div className="landing-page-splitgrid">
+            <div className="landing-page-col">
+              {/*<img src="FILL WITH SCREENSHOT" />*/}
+            </div>
+            <div className="landing-page-col">
+              <div className="landing-page-col-text">
+                <p className="landing-page-txt">Schedule workouts.</p>
+                <p className="landing-page-txt">Add to client calendars.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*EXERCISE PREVIEW*/}
+        <div className="landing-page-preview">
+          <div className="landing-page-splitgrid">
+            <div className="landing-page-col">
+              <div className="landing-page-col-text">
+                <p className="landing-page-txt">Plan and track exercises.</p>
+                <p className="landing-page-txt">Star exercises for simple re-use.</p>
+                <p className="landing-page-txt">Craft workouts with ease.</p>
+              </div>
+            </div>
+            <div className="landing-page-col">
+              {/*<img src="FILL WITH SCREENSHOT" />*/}
+            </div>
+          </div>
+        </div>
+        {/*CLIENT PREVIEW*/}
+        <div className="landing-page-preview">
+          <div className="landing-page-splitgrid">
+            <div className="landing-page-col">
+              {/*<img src="FILL WITH SCREENSHOT" />*/}
+            </div>
+            <div className="landing-page-col">
+              <div className="landing-page-col-text">
+                <p className="landing-page-txt">View client's progression.</p>
+                <p className="landing-page-txt">Utilize exercise history data to optimize regimens.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*INCENTIVE PREVIEW*/}
+        <div className="landing-page-preview">
+          <div className="landing-page-splitgrid">
+            <div className="landing-page-col">
+              <div className="landing-page-col-text">
+                <p className="landing-page-txt">Self-incentivize your motivation.</p>
+                <p className="landing-page-txt">Declare a goal.</p>
+                <p className="landing-page-txt">Set a deadline.</p>
+                <p className="landing-page-txt">Send ETH to a smart contract.</p>
+                <p className="landing-page-txt">Hold yourself accountable.</p>
+              </div>
+            </div>
+            <div className="landing-page-col">
+              {/*<img src="FILL WITH SCREENSHOT" />*/}
+            </div>
+          </div>
+        </div>
+        <p className="landing-page-footer">Copyright &copy; 2018 BoostPT. All Rights Reserved.</p>
       </div>
     )
   }
